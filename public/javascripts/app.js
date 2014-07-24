c233159211c802db97197141dba648968cd9c7b2
tae yoon <tyoon@hotmail.com>
2014-07-24 18:29:03 -0400

    added bower components and testing code

---
+++ public/javascripts/app.js
@@ -0,0 +1,19 @@
+var wrapper = document.getElementById("main-pad"),
+    chooseButton = wrapper.querySelector("[data-action=choose]"),
+    saveButton = wrapper.querySelector("[data-action=launch]");
+
+    chooseButton.addEventListener("click", function (event) {
+    	$.get("http://127.0.0.1:9233/WelchAllyn/Device/GetDevices",function(data,status){
+    			alert("Data: " + data + "\nStatus: " + status);
+  			});
+
+    	// $.ajax({
+     //    url : "http://127.0.0.1:9233/WelchAllyn/Device/GetDevices",
+     //    dataType:"jsonp",
+     //    jsonp:"mycallback",
+     //    success:function(data)
+     //    {
+     //        alert("Data: " + data + "\nStatus: " + status);
+     //    }
+    //});
+	});
